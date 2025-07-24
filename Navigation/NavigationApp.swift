aa46b2dc9b26a27f8f3be068ed8a4848b2aa468d
Reese <reesemiyowalther@gmail.com>
2025-07-24 06:57:52 -1000

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by rwalther28 on 7/24/25.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
