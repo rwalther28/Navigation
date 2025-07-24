2c143c7de3d6b9d64e6113cfcb5e3ee46de533fb
Reese <reesemiyowalther@gmail.com>
2025-07-24 07:21:31 -1000

    Added a second view and changed the navigation title

--- Navigation/ContentView.swift
+++ Navigation/ContentView.swift
@@ -12,10 +12,14 @@ struct ContentView: View {
             NavigationStack {
                 VStack {
                     Text("This is the root view 🌳")
-                    NavigationLink(destination: Text("You've arrived to the Second View 🎊")) {
+                    NavigationLink(destination: SecondView()) {
                         Text("Click me!")
                     }
+
                 }
+                .navigationTitle("Home")
+                .navigationBarTitleDisplayMode(.inline)
+                .navigationBarHidden(true)
             }
         }
     }
